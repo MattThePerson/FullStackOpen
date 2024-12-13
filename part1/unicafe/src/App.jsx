71da8534b796c17c5dca68a49cc06276927acee9
Matt Stirling <stirlingmattmarkus@gmail.com>
2024-12-13 21:50:58 +0200

    [part1] unicafe step 1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,37 +1,33 @@
 import { useState } from 'react'
-import './App.css'
 
-let viteLogo = 'src/assets/pulse.svg'
-let reactLogo = 'src/assets/pulse.svg'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+function Button({type, onClick}) {
+  console.log(`Clicked "${type}" Button`)
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <button onClick={onClick}>{type}</button>
   )
 }
 
+const App = () => {
+  // save clicks of each button to its own state
+
+  console.log('Initialized App');
+  
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h2>Give feedback</h2>
+      <Button type="good" onClick={() => {setGood(good+1)}}></Button>
+      <Button type="neutral" onClick={() => {setNeutral(neutral+1)}}></Button>
+      <Button type="bad" onClick={() => {setBad(bad+1)}}></Button>
+      <h2>Statistics</h2>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+    </div>
+  )
+}
 
 export default App

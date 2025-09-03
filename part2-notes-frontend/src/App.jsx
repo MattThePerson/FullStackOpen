1fb1389d38b51397714433e50fe2735c7b3b989e
Matt Stirling <stirlingmattmarkus@gmail.com>
2025-09-03 14:48:46 +0300

    notes: learned about forums and state

--- part2-notes-frontend/src/App.jsx
+++ part2-notes-frontend/src/App.jsx
@@ -1,14 +1,61 @@
+import { useState } from 'react'
 import Note from './components/Note'
 
-const App = ({ notes }) => {
+const App = (props) => {
+
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("note placeholder..");
+  const [showAll, setShowAll] = useState(false);
+  
+  // onSubmit
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: String(notes.length + 1),
+    }
+    console.log("SUBMITTING NOTE:", e.target.querySelector("input").value);
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
+  }
+
+  // handleInputValueChange
+  const handleInputValueChange = (e) => {
+    console.log(e.target.value);
+    setNewNote(e.target.value);
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(n => n.important);
+  
+  
+  // JSX
   return (
     <div>
+      
       <h1>Notes</h1>
+
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note => 
           <Note key={note.id} note={note} />
         )}
       </ul>
+
+      <form onSubmit={onSubmit}>
+        <input
+          value={newNote}
+          onChange={handleInputValueChange}
+        />
+        <button type="submit">save</button>
+      </form>
+      
     </div>
   )
 }

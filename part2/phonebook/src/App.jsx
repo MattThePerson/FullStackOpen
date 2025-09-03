0ea10a6540e2c389ce6125b6a19d44d4b7a92b50
Matt Stirling <stirlingmattmarkus@gmail.com>
2025-09-03 17:54:23 +0300

    [part 2.9] The Phonebook Step 4: contact filtering

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -5,15 +5,28 @@ function isProbablyAPhoneNumber(x) {
     return !isNaN(x.replaceAll('-', ''))
 }
 
+// filterContacts
+function filterContacts(contacts, filter) {
+    const filter_lc = filter.toLowerCase();
+    return contacts.filter(c => 
+        c.name.toLowerCase().includes(filter_lc)
+    );
+}
+
 // App
 const App = () => {
 
     // State
     const [persons, setPersons] = useState([
-        { id: 0, name: 'Arto Hellas', number: 1234, }
+        { id: 0, name: 'Matti Mustonen', number: "045-1234" },
+        { id: 1, name: 'Arto Hellas', number: '040-123456'},
+        { id: 2, name: 'Ada Lovelace', number: '39-44-5323523'},
+        { id: 3, name: 'Dan Abramov', number: '12-43-234345'},
+        { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' }
     ])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
+    const [nameFilter, setNameFilter] = useState('')
 
     // handleFormSubmit
     const handleFormSubmit = e => {
@@ -38,16 +51,28 @@ const App = () => {
         setNewName('');
         setNewNumber('');
     }
+
+    // filter contacts
+    const personsToShow = filterContacts(persons, nameFilter);
     
     // JSX
     return (
         <div>
 
-            <div>debug: {newName}</div>
+            {/* <div>debug: {nameFilter}</div> */}
             
-            <h2>Phonebook</h2>
+            <h1>Phonebook</h1>
+
+            {/* search */}
+            <div>Filter names:
+                <input
+                    value={nameFilter}
+                    onChange={e => setNameFilter(e.target.value)}
+                />
+            </div>
 
             {/* forms */}
+            <h2>Add new contact</h2>
             <form onSubmit={handleFormSubmit}>
                 <div>
                     name: <input
@@ -67,8 +92,8 @@ const App = () => {
             </form>
 
             {/* numbers */}
-            <h2>Numbers</h2>
-            {persons.map((p) => 
+            <h2>Contacts</h2>
+            {personsToShow.map((p) => 
                 <div key={p.name}>
                     {p.name} {p.number}
                 </div>

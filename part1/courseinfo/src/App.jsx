b6d00224150b71a73fd83451040cec6047578eec
Matt Stirling <stirlingmattmarkus@gmail.com>
2025-09-03 12:18:10 +0300

    [part 2.5] converted Course into module

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,48 +1,4 @@
-
-function Header({ course }) {
-    return (
-        <h2>{course.name}</h2>
-    )
-}
-
-function Content({ course }) {
-    // list needs unique 'key', component won't recieve key as prop
-    return (
-        <div>
-            {course.parts.map(part =>
-                <Part key={part.name} part={part} />
-            )}
-        </div>
-    )
-}
-
-function Total({ course }) {
-    const total = course.parts.reduce((acc, curr, idx) => {
-        if (acc.exercises) acc = acc.exercises;
-        // console.log(idx, acc, curr.exercises);
-        return acc + curr.exercises;
-    });
-    return (
-        <strong><p>Number of exercises {total}</p></strong>
-    )
-}
-
-function Part({ part }) {
-    return (
-        <p>{part.name} {part.exercises} </p>
-    )
-}
-
-function Course({ course }) {
-    console.log("rendering course");
-    return (
-        <div>
-            <Header course={course} />
-            <Content course={course} />
-            <Total course={course} />
-        </div>
-    )
-}
+import { Course } from "./Course";
 
 const App = () => {
     const courses = [

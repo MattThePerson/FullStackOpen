12b522d087695fb5d20af938100fa3f4606760c1
Matt Stirling <stirlingmattmarkus@gmail.com>
2025-09-05 16:33:34 +0300

    [part 2.17] Phonebook step 12: added error handling for when user deleted before update/delete

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,8 +3,10 @@ import { useState, useEffect } from 'react'
 import ContactsList from './components/ContactsList';
 import ContactAdderForm from './components/ContactAdderForm';
 import ContactFilter from './components/ContactFilter';
+import Notification from './components/Notification';
 
 import personsService from './services/persons';
+import ErrorMessage from './components/ErrorMessage';
 
 
 // isProbablyAPhoneNumber
@@ -16,11 +18,15 @@ function isProbablyAPhoneNumber(x) {
 // App
 const App = () => {
 
+    // app state
     const [persons, setPersons] = useState([]);
     const [newName, setNewName] = useState('');
     const [newNumber, setNewNumber] = useState('');
     const [nameFilter, setNameFilter] = useState('');
 
+    const [notification, setNotification] = useState(null);
+    const [errorMessage, setErrorMessage] = useState(null);
+    
 
     /* fetch initial persons */
     useEffect(() => {
@@ -34,7 +40,7 @@ const App = () => {
     console.debug(`amount of persons ${persons.length}`);
 
     
-    // handleFormSubmit
+    /* handleFormSubmit: new contact form */
     const handleFormSubmit = (e) => {
         e.preventDefault();
 
@@ -47,8 +53,9 @@ const App = () => {
             name: newName,
             number: newNumber,
         };
+
         
-        // update phone number
+        // update existing contact
         const existingPerson = persons.find(x => x.name.toLowerCase() === newName.toLowerCase());
         if (existingPerson) {
             if (window.confirm(`${newName} already exists in the phonebook (id: ${existingPerson.id}). Update phonenumber to: ${newNumber}?`)) {
@@ -56,10 +63,20 @@ const App = () => {
                     .update(existingPerson.id, personObj)
                     .then(returnedPerson => {
                         setPersons(persons.map(p => (p.id === returnedPerson.id) ? returnedPerson : p))
+                        setNewName('');
+                        setNewNumber('');
+                        setNotification(`updated phone number for "${returnedPerson.name}" (new number: ${returnedPerson.number})`)
+                        setTimeout(() => setNotification(null), 3000);
+                    })
+                    .catch(() => {
+                        setPersons(persons.filter(p => p.id !== existingPerson.id));
+                        setErrorMessage(`contact "${existingPerson.name}" appears to have been deleted, please resubmit contact`)
+                        setTimeout(() => setErrorMessage(null), 4000);
                     })
             }
             return;
         }
+        
 
         // add new contact
         personsService
@@ -68,24 +85,32 @@ const App = () => {
                 setPersons(persons.concat(returnedPerson))
                 setNewName('');
                 setNewNumber('');
+                setNotification(`added contact "${returnedPerson.name}" with phone number "${returnedPerson.number}" and id "${returnedPerson.id}"`)
+                setTimeout(() => setNotification(null), 4000);
             })
+        
     }
 
     
-    // deleteContact
+    /* deleteContact */
     const deleteContact = (id) => {
         const person = persons.find(p => p.id === id);
 
         if (!window.confirm(`Delete contact "${person.name}" with id: ${id}?`)) {
             console.debug("not deleting contact");
-            return
+            return;
+        }
+        
+        const updatePersonsAfterDeletion = () => {
+            setPersons(persons.filter(p => p.id !== id));
+            setNotification(`deleted contact "${person.name}" (id: ${person.id})`)
+            setTimeout(() => setNotification(null), 3000)
         }
         
         personsService
             .deletePerson(id)
-            .then(() => {
-                setPersons(persons.filter(p => p.id !== id));
-            })
+            .then(updatePersonsAfterDeletion)
+            .catch(updatePersonsAfterDeletion)
     }
     
 
@@ -100,6 +125,9 @@ const App = () => {
         <div>
 
             <h1>Phonebook</h1>
+
+            <Notification message={notification} />
+            <ErrorMessage message={errorMessage} />
 
             <ContactFilter
                 nameFilter={nameFilter}
---
+++ part2/phonebook/src/components/ErrorMessage.jsx
@@ -0,0 +1,24 @@
+
+
+const ErrorMessage = ({message}) => {
+
+    const style = {
+        color: 'red',
+        background: 'lightgrey',
+        fontSize: '20px',
+        borderStyle: 'solid',
+        borderRadius: '5px',
+        padding: '10px',
+        marginBottom: '10px',
+    }
+    
+    if (message === null) return null;
+    
+    return (
+        <div style={style}>
+            {message}
+        </div>
+    )
+}
+
+export default ErrorMessage;

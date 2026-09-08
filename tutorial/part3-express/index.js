2b708ce02eaa368d284d436cdb93d63a0785b839
Matt Stirling <stirlingmattmarkus@gmail.com>
2026-09-08 17:41:56 +0300

    [part3] did express hello world and renamed notes-frontned folder

---
+++ tutorial/part3-express/index.js
@@ -0,0 +1,80 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json()) // use express' json-parser
+
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+
+const getNoteId = () => {
+    const id = notes.length == 0
+        ? 0
+        : Math.max(...notes.map(n => Number(n.id)))
+    return String(id+1);
+}
+
+app.get("/", (req, res) => {
+    res.send('<h1>Hello World!</h1>');
+})
+
+app.get("/api/notes", (req, res) => {
+    res.json(notes);
+})
+
+app.get("/api/notes/:id", (req, res) => {
+    const id = req.params.id;
+    const note = notes.find(note => note.id === id);
+    if (note) {
+        res.json(note);
+    } else {
+        res.statusMessage = "no note with that id";
+        res.status(404).end();
+    }
+})
+
+app.delete("/api/notes/:id", (req, res) => {
+    const id = req.params.id;
+    notes = notes.filter(note => note.id !== id);
+    res.status(204).end();
+})
+
+app.post("/api/notes", (req, res) => {
+    const body = req.body;
+    console.log(body);
+
+    if (!body.content) {
+        return res.status(400).json({
+            error: "content missing",
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        id: getNoteId(),
+    }
+
+    notes = notes.concat(note);
+    res.json(note);
+})
+
+// start
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})

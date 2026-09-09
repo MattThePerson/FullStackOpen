5af76f220edf18e7a3894c367f67c0ac650518b3
Matt Stirling <stirlingmattmarkus@gmail.com>
2026-09-09 12:43:30 +0300

    [part 3.6] delete, post, error checking

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 let persons = [
     {
@@ -24,8 +25,47 @@ let persons = [
     }
 ]
 
+function getNewId() {
+    return String(Math.floor(Math.random() * Math.pow(2, 32)));
+}
+
 app.get("/api/persons", (req, res) => {
     res.json(persons);
+})
+
+app.get("/api/persons/:id", (req, res) => {
+    const id = req.params.id;
+    const person = persons.find(p => p.id === id);
+    if (!person) {
+        return res.status(404).json({
+            error: "no person with that id"
+        })
+    }
+    res.json(person);
+})
+
+app.delete("/api/persons/:id", (req, res) => {
+    const id = req.params.id;
+    persons = persons.filter(p => p.id !== id);
+    res.status(204).end();
+})
+
+app.post("/api/persons", (req, res) => {
+    const body = req.body;
+    if (!body.name || !body.number) {
+        return res.status(400).json({error: "name or number missing for contact"})
+    }
+    const personWithName = persons.find(p => p.name === body.name);
+    if (personWithName) {
+        return res.status(400).json({error: "contact already exists with that name"})
+    }
+    const person = {
+        id: getNewId(),
+        name: body.name,
+        number: body.number,
+    }
+    persons = persons.concat(person);
+    res.json(person);
 })
 
 app.get("/info", (req, res) => {

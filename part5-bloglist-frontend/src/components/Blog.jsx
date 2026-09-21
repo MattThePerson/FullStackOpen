52350714a9cab1658223f661994fc99bc8ac7263
Matt Stirling <stirlingmattmarkus@gmail.com>
2026-09-21 15:37:42 +0300

    [part 5.7] added 'view' button to blogs

--- part5-bloglist-frontend/src/components/Blog.jsx
+++ part5-bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,39 @@
-const Blog = ({ blog }) => (
-    <div>
-        {blog.title} {blog.author}
-    </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+
+    const [viewDetails, setViewDetails] = useState(false)
+
+    const toggleViewDetails = () => setViewDetails(!viewDetails)
+
+    const detailsHiddenStyle = { display: viewDetails ? 'none' : 'block' }
+    const detailsShownStyle =  { display: viewDetails ? 'block' : 'none' }
+
+    const blogItemStyle = {
+        border: '1px solid black',
+        width: '25rem',
+    }
+
+    return (
+        <div style={blogItemStyle}>
+            <b>{blog.title}</b> by <i>{blog.author}</i>
+            <button style={detailsHiddenStyle} onClick={toggleViewDetails} >view</button>
+            <button style={detailsShownStyle} onClick={toggleViewDetails} >hide</button>
+
+            {/* blog details */}
+            <div style={detailsShownStyle}>
+                {blog.url}
+                <div>
+                    {blog.likes}
+                    <button
+                        onClick={() => console.log('me like')}
+                    >like</button>
+                </div>
+                {blog.user.username}
+            </div>
+
+        </div>
+    )
+}
 
 export default Blog
